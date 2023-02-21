4a98afb26c808441025574800003bec3dad14320
Viktors <viktorsseils21@gmail.com>
2023-02-21 18:45:23 +0200

    code changes

--- app/src/main/java/com/main/simplejsonlistwuthcustomui/MainActivity.java
+++ app/src/main/java/com/main/simplejsonlistwuthcustomui/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.ListAdapter;
 
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,6 +24,8 @@ public class MainActivity extends ListActivity {
     String url ="https://jsonplaceholder.typicode.com/posts";
 
     JSONArray blogposts = null;
+
+    private ProgressDialog progressDialog;
 
     ArrayList<HashMap<String,String>> blogPostList;
 
@@ -38,5 +44,56 @@ public class MainActivity extends ListActivity {
 
             }
         });
+    }
+
+    private class GetPostList extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("downloading data, please wait!");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            ServiceHandler sh = new ServiceHandler(url);
+            String jsonString = sh.makeServiceCall();
+            if(jsonString!=null){
+                try {
+                    blogposts = new JSONArray(jsonString);
+
+                    for(int i= 0; i<blogposts.length();i++){
+                        JSONObject b = blogposts.getJSONObject(i);
+                        String title = b.getString(GV.TITLE);
+
+                        HashMap<String,String> oneBlogPost = new HashMap<>();
+                        oneBlogPost.put(GV.TITLE,title);
+                        oneBlogPost.put(GV.BODY,b.getString(GV.BODY));
+
+                        blogPostList.add(oneBlogPost);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+
+            if(progressDialog.isShowing()){
+                progressDialog.dismiss();
+            }
+        }
+
+
     }
 }
